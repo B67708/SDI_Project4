16766de9fbb005d349c9d001c551ad355a1c8b6e
John Cardiff-Winchell <johnwinchell86@fullsail.edu>
2013-02-25 23:48:14 -0500

    Initial commit

---
+++ CardiffWinchell_John_assignment4.js
@@ -0,0 +1,8 @@
+// John Cardiff-Winchell
+// SDI 1302
+// Project 4
+
+// Does a string follow a pattern like a phone number?
+
+// Function determines if string follows pattern like a phone number.
+// var checkPhoneNumber = function(){
